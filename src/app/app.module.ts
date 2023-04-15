038a7afd6f2fdbaae2c3771326891e5fa2568a01
Rodrigo Neves <{ID}+{username}@users.noreply.github.com>
2023-04-15 10:41:47 -0300

    feat(environments): created environments and import angular Firestore modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCVYwQ6Wf2aXqTEFxDy8j7gJmD9KZMec84",
+        authDomain: "kanbanfire-4b711.firebaseapp.com",
+        databaseURL: "https://kanbanfire-4b711-default-rtdb.firebaseio.com",
+        projectId: "kanbanfire-4b711",
+        storageBucket: "kanbanfire-4b711.appspot.com",
+        messagingSenderId: "701718487577",
+        appId: "1:701718487577:web:3ae3fad7a21894aa820088",
+    }
+  };
